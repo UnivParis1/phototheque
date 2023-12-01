d88698cdd5b5a4f53a3bd35d8892fdc5a6a445a4
plevy <plevy@up1>
2023-12-01 17:46:48 +0100

    - début divers nettoyage code - résolution bug : forcer reinitialisation cache à la modification des appartenances groupes.

--- plugins/casusers/include/auth.inc.php
+++ plugins/casusers/include/auth.inc.php
@@ -2,14 +2,16 @@
 
 defined('CASU_PATH') or die('Hacking attempt!');
 
-function casu_try_log_user($success, $cas_user) {
-    if ($success === true) {
+function casu_try_log_user($success, $cas_user)
+{
+    if ($success === true)
+    {
         return true;
     }
 
 //we want user['id'] with casu_id
-    if (!($userid = casu_get_userid($cas_user['id']))) {
-
+    if (!($userid = casu_get_userid($cas_user['id'])))
+    {
         $userid = casu_register_user($cas_user);
     }
 
@@ -18,22 +20,27 @@ function casu_try_log_user($success, $cas_user) {
     return true;
 }
 
-function casu_get_userid($cas_user) {
+function casu_get_userid($cas_user)
+{
     $query = 'SELECT user_id FROM '
             . USER_INFOS_TABLE .
             ' WHERE casu_id LIKE "'
             . $cas_user . '"';
     $result = pwg_query($query);
 
-    if (pwg_db_num_rows($result) == 0) {
+    if (pwg_db_num_rows($result) == 0)
+    {
         
-    } else {
+    } 
+    else 
+    {
         list($user_id) = pwg_db_fetch_row($result);
         return $user_id;
     }
 }
 
-function casu_register_user($cas_user) {
+function casu_register_user($cas_user)
+{
     global $conf;
     $insert = array(
         $conf['user_fields']['username'] => pwg_db_real_escape_string($cas_user['user']),
@@ -52,21 +59,24 @@ SELECT id
 ;';
     $result = pwg_query($query);
     $inserts = array();
-    while ($row = pwg_db_fetch_assoc($result)) {
+    while ($row = pwg_db_fetch_assoc($result))
+    {
         $inserts[] = array(
             'user_id' => $user_id,
             'group_id' => $row['id']
         );
     }
-    if (count($inserts) != 0) {
+    if (count($inserts) != 0) 
+    {
         mass_inserts(USER_GROUP_TABLE, array('user_id', 'group_id'), $inserts);
     }
 
     $override = array();
-    if ($conf['browser_language'] and $language = get_browser_language()) {
+    if ($conf['browser_language'] and $language = get_browser_language())
+    {
         $override['language'] = $language;
-        $override['casu_id'] = $cas_user['id'];
     }
+    $override['casu_id'] = $cas_user['id'];
 
     create_user_infos($user_id, $override);
 
@@ -82,22 +92,34 @@ SELECT id
     return $user_id;
 }
 
-function casu_get_casu_groups($attributes) {
+function casu_get_casu_groups($attributes)
+{
     $casu_groups = array();
     $confcasu = conf_get_param('casu', array());
-    foreach ($confcasu['casu_groups'] as $group_attr => $regexp) {
-        if (is_array($attributes[$group_attr])) {
-            foreach ($attributes[$group_attr] as $group) {
-                if (empty($regexp)) {
+    foreach ($confcasu['casu_groups'] as $group_attr => $regexp)
+    {
+        if (is_array($attributes[$group_attr]))
+        {
+            foreach ($attributes[$group_attr] as $group)
+            {
+                if (empty($regexp))
+                {
                     $casu_groups[] = $group;
-                } elseif (preg_match($regexp, $group, $groupm)) {
+                }
+                elseif (preg_match($regexp, $group, $groupm))
+                {
                     $casu_groups[] = $groupm[1];
                 }
             }
-        } else {
-            if (empty($regexp)) {
+        }
+        else
+        {
+            if (empty($regexp))
+            {
                 $casu_groups[] = $attributes[$group_attr];
-            } elseif (preg_match($regexp, $attributes[$group_attr], $groupm)) {
+            }
+            elseif (preg_match($regexp, $attributes[$group_attr], $groupm))
+            {
                 $casu_groups[] = $groupm[1];
             }
         }
@@ -105,7 +127,9 @@ function casu_get_casu_groups($attributes) {
     return($casu_groups);
 }
 
-function casu_register_groups($user_id, $casu_groups) {
+function casu_register_groups($user_id, $casu_groups)
+{
+    $update_groups = false ;
 
     //we want the list of all *casu_* groups the user is member of
     $query = 'SELECT DISTINCT  gr.id '
@@ -117,22 +141,28 @@ function casu_register_groups($user_id, $casu_groups) {
     $result = pwg_query($query);
 
     $groups_old = array();
-    if (pwg_db_num_rows($result) > 0) {
-        while (list($id) = pwg_db_fetch_row($result)) {
+    if (pwg_db_num_rows($result) > 0)
+    {
+        while (list($id) = pwg_db_fetch_row($result))
+        {
             $groups_old[] = $id;
         }
     }
 
     //now we begin
     $groups_add = array();
-    foreach ($casu_groups as $group) {
+    foreach ($casu_groups as $group)
+    {
         $query = 'SELECT id,name FROM ' . GROUPS_TABLE
                 . ' WHERE name LIKE \'casu_' . pwg_db_real_escape_string($group) . '\';';
         $result = pwg_query($query);
-        if (pwg_db_num_rows($result) == 1) {
+        if (pwg_db_num_rows($result) == 1)
+        {
             list($id) = pwg_db_fetch_row($result);
             $groups_add[] = $id;
-        } elseif (pwg_db_num_rows($result) == 0) {
+        }
+        elseif (pwg_db_num_rows($result) == 0)
+        {
             $query = 'INSERT INTO ' . GROUPS_TABLE .
                     ' (name) VALUES (\'casu_'
                     . pwg_db_real_escape_string($group) . '\');';
@@ -150,20 +180,30 @@ function casu_register_groups($user_id, $casu_groups) {
     $groups_reg = array_diff($groups_add, $groups_old);
 
     $inserts = array();
-    foreach ($groups_reg as $group) {
+    foreach ($groups_reg as $group)
+    {
         $inserts[] = array(
             'user_id' => $user_id,
             'group_id' => $group
         );
     }
-    if (count($inserts) != 0) {
+    if (count($inserts) != 0)
+    {
         mass_inserts(USER_GROUP_TABLE, array('user_id', 'group_id'), $inserts);
+        $update_groups = true ;
     }
-    foreach ($groups_remove as $group) {
+    foreach ($groups_remove as $group)
+    {
         $query = 'DELETE FROM ' . USER_GROUP_TABLE .
                 ' WHERE user_id = \'' . $user_id . '\'' .
                 ' AND group_id = \'' . $group . '\''
         ;
         pwg_query($query);
+        $update_groups = true;
+    }
+    if($update_groups)
+    {
+        include_once 'admin/include/functions.php';
+        invalidate_user_cache();
     }
 }
