a6256df1414011e100613f52d6ebb4a2a96a8eb6
plevy <plevy@up1>
2023-12-01 19:53:59 +0100

    compatibilité thèmes sans menu identification rapide

--- plugins/casusers/include/public_events.inc.php
+++ plugins/casusers/include/public_events.inc.php
@@ -6,7 +6,8 @@ defined('CASU_PATH') or die('Hacking attempt!');
 /*
  * prepare things to the connexion menu :
  */
-function casu_blockmanager($menu_ref_arr) {
+function casu_blockmanager($menu_ref_arr)
+{
     global $template;
     $menu = &$menu_ref_arr[0];
 
@@ -25,8 +26,9 @@ function casu_blockmanager($menu_ref_arr) {
 /*
  * we want to replace completely the form part in identification_menubar.tpl as CAS becomme the only authentification available
  */
-function casu_add_menubar_buttons_prefilter($content, &$smarty) {
-    $search = '#(<form.*/form>)#is';
+function casu_add_menubar_buttons_prefilter($content, $smarty)
+{
+    $search = '#(<form[^>]*action="{\$U_LOGIN}".*/form>)#is';
     $replace = file_get_contents(CASU_PATH . 'template/identification_menubar.tpl');
     return preg_replace($search, $replace, $content);
 }
